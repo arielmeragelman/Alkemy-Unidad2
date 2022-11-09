98d3b85e3e1a419336436cca5564dc5b305d92b9
Sebastian Meragelman <arielmeragelman@gmail.com>
2022-11-09 18:43:37 -0300

    se cambia a dotenv

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from decouple import config
+import os
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
+Host = os.getenv('MYSQL_HOST')
+Puerto = os.getenv('MYSQL_PORT')
+Usuario = os.getenv('MYSQL_USER')
+Password = os.getenv('MYSQL_PWD')
 
 print("Los datos de la conexion son")
-print(f"Host: {config('MYSQL_HOST')} PUERTO: {config('MYSQL_PORT')} USUARIO: {config('MYSQL_USER')} PASSWORD: {config('MYSQL_PWD')}   ")
+print(f"Host: {Host} PUERTO: {Puerto} USUARIO: {Usuario} PASSWORD: {Password}   ")
